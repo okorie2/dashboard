cf5d8cd2a659cb204a88b3607be90d6b10b1b173
Okorie Emmanuella <ellahokorie@gmail.com>
2022-02-06 17:16:38 +0100

    responsive

--- src/Componets/Loader.js
+++ src/Componets/Loader.js
@@ -4,7 +4,7 @@ import { RevolvingDot } from "react-loader-spinner";
 function Loader(props) {
   return (
     <div className="loader">
-      <RevolvingDot type="TailSpin" color="#fff" height={100} width={100} />
+      <RevolvingDot type="TailSpin" color="black" height={100} width={100} />
     </div>
   );
 }
