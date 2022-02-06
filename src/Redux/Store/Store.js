a4c3aedd2ef5806207687d0bf836979ef622794d
Okorie Emmanuella <ellahokorie@gmail.com>
2022-02-06 17:35:13 +0100

    responsive

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "../Reducers/index";
 
 const Store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk, logger))
+  composeWithDevTools(applyMiddleware(thunk))
 );
 export default Store;
